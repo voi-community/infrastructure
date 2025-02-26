2537306055b0cd37a77065783976980e34915f98
Kieran O'Neill <hello@kieranoneill.com>
2025-02-26 17:04:53 +0000

    feat: add github pages records and vips subdomain

--- workspaces/boundary/modules/domain/main.tf
+++ workspaces/boundary/modules/domain/main.tf
@@ -5,3 +5,47 @@
 resource "digitalocean_domain" "domain" {
   name = var.domain
 }
+
+#####
+# dns records
+#####
+
+// github pages
+
+resource "digitalocean_record" "github_pages_1" {
+  domain = digitalocean_domain.domain.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.108.153"
+}
+
+resource "digitalocean_record" "github_pages_2" {
+  domain = digitalocean_domain.domain.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.109.153"
+}
+
+resource "digitalocean_record" "github_pages_3" {
+  domain = digitalocean_domain.domain.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.110.153"
+}
+
+resource "digitalocean_record" "github_pages_4" {
+  domain = digitalocean_domain.domain.id
+  type   = "A"
+  name   = "@"
+  value  = "185.199.111.153"
+}
+
+// subdomain: vips
+
+resource "digitalocean_record" "vips_subdomain" {
+  domain = digitalocean_domain.domain.id
+  name   = "vips"
+  ttl    = 21600
+  type   = "CNAME"
+  value  = "voi-community.github.io."
+}
